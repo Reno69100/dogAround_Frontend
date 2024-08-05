87156200ed036d9b65af3b92263492285b6dd5de
Mathieu <m.hembert@outlook.fr>
2024-08-05 17:44:24 +0200

    icone avata mapscrenn location utilisateur

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -3,6 +3,7 @@ import {
     StyleSheet,
     View,
     Dimensions,
+    Image,
 } from "react-native";
 
 import MapView, { Marker } from 'react-native-maps';
@@ -14,7 +15,7 @@ import { importPlaces } from '../reducers/places'
 export default function MapScreen({ navigation }) {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user.value); //Recuperation paramètres de l'utilsateur stocké dans le STORE
-    const [currentPosition, setCurrentPosition] = useState({"latitude": 50.3026331775362, "longitude": 2.796574970709902}); //Déclaration état contenant la posittion de l'utisateur
+    const [currentPosition, setCurrentPosition] = useState({ "latitude": 50.3026331775362, "longitude": 2.796574970709902 }); //Déclaration état contenant la posittion de l'utisateur
 
     useEffect(() => {
         //Demande autorisation partage location du téléphone
@@ -31,7 +32,7 @@ export default function MapScreen({ navigation }) {
                     });
             }
         })();
-        
+
 
         //Récupération des points d'intérêts autour de l'utilisateur
         /* const params = {
@@ -58,7 +59,13 @@ export default function MapScreen({ navigation }) {
                     longitudeDelta: 0.1,
                 }}
             >
-                {currentPosition && <Marker style={styles.maposition} image={require("../assets/avatars/chien_1.png")} coordinate={currentPosition} title="Ma position" pinColor="#fecb2d" />}
+                {currentPosition && 
+                <Marker style={styles.maposition} coordinate={currentPosition} title="Ma position" pinColor="#fecb2d">
+                    <Image
+                        source={require("../assets/avatars/chien_1.png")}
+                        style={{ width: 25, height: 25, resizeMode: "contain" }}
+                    />
+                </Marker>}
                 {/* {markers} */}
             </MapView>
         </View>
@@ -75,7 +82,6 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
     },
     maposition: {
-        width: 20,
-        height: 20,
+
     },
 });
