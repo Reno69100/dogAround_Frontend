29ef0eb5f8d28000d0aab61f1647bbaba3c5d17c
Mathieu <m.hembert@outlook.fr>
2024-08-05 12:17:17 +0200

    rajout defauts assets

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,14 +2,49 @@ import React, { useState } from "react";
 import {
     StyleSheet,
     View,
-    Text,
+    Dimensions,
 } from "react-native";
 
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+
 export default function MapScreen({ navigation }) {
+    const [currentPosition, setCurrentPosition] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const result = await Location.requestForegroundPermissionsAsync();
+            const status = result?.status;
+
+            if (status === 'granted') {
+                Location.watchPositionAsync({ distanceInterval: 10 },
+                    (location) => {
+                        setCurrentPosition(location.coords);
+                    });
+            }
+        })();
+
+        /* fetch(`${BACKEND_ADDRESS}/places/${user.nickname}`)
+            .then((response) => response.json())
+            .then((data) => {
+                data.result && dispatch(importPlaces(data.places));
+            }); */
+    }, []);
 
     return (
         <View style={styles.container}>
-
+            <MapView
+                style={styles.map}
+                initialRegionregion={{
+                    latitude: currentPosition.latitude,
+                    longitude: currentPosition.longitude,
+                    latitudeDelta: 0.1,
+                    longitudeDelta: 0.1,
+                }}
+            >
+                {currentPosition && <Marker /* image={require("./assets/avatar.png")} */ coordinate={currentPosition} title="Ma position" pinColor="#fecb2d" />}
+                {markers}
+            </MapView>
         </View>
     );
 }
@@ -17,15 +52,10 @@ export default function MapScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-        height: "100%",
-        width: "100%",
         flex: 1,
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        backgroundColor: "#E8E9ED",
     },
-    text: {
-        color: "#BB7E5D",
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
     },
 });
