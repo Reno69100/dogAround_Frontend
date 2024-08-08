75b17151d67333adc3343618233ab5f6155b5968
Gaspard <Your email>
2024-08-08 11:04:08 +0200

    change avatar good

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -63,6 +63,7 @@ export default function SignUpScreen({ navigation }) {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        avatar: 39,
         email: email,
         pseudo: pseudo,
         password: password,
